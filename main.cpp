d13d9bb5d125e29ee856a5d2cdcadcd237269add
Nguyen Nhan <nhanhoa21012002@gmail.com>
2021-04-01 08:47:19 +0700

    operator overload

--- main.cpp
+++ main.cpp
@@ -1,60 +1,47 @@
 #include <string>
 #include <iostream>
- 
+
 class Ball
 {
 private:
-	std::string m_color;
-	double m_radius;
- 
+    std::string m_color{"black"};
+    double m_radius{};
+
 public:
-	// Default constructor with no parameters
-	Ball()
-	{
-		m_color = "black";
-		m_radius = 10.0;
-	}
- 
-	// Constructor with only color parameter (radius will use default value)
-	Ball(const std::string &color)
-	{
-		m_color = color;
-		m_radius = 10.0;
-	}
- 
-	// Constructor with only radius parameter (color will use default value)
-	Ball(double radius)
-	{
-		m_color = "black";
-		m_radius = radius;
-	}
- 
-	// Constructor with both color and radius parameters
-	Ball(const std::string &color, double radius)
-	{
-		m_color = color;
-		m_radius = radius;
-	}
- 
-	void print()
-	{
-		std::cout << "color: " << m_color << ", radius: " << m_radius << '\n';
-	}
+    // Constructor with only radius parameter (color will use default value)
+    Ball(double radius) : m_radius{radius}
+    {
+    }
+
+    // Constructor with both color and radius parameters
+    Ball(const std::string &color = "black", double radius = 10) : m_radius{radius}, m_color{color}
+    {
+    }
+
+    void print()
+    {
+        std::cout << "color: " << m_color << ", radius: " << m_radius << '\n';
+    }
+    friend std::ostream &operator<<(std::ostream &out, const Ball &ball);
 };
- 
+std::ostream &operator<<(std::ostream &out, const Ball &ball)
+{
+    std::cout << "color: " << ball.m_color << ", radius: " << ball.m_radius << '\n';
+    return out;
+}
 int main()
 {
-	Ball def;
-	def.print();
- 
-	Ball blue{ "blue" };
-	blue.print();
-	
-	Ball twenty{ 20.0 };
-	twenty.print();
-	
-	Ball blueTwenty{ "blue", 20.0 };
-	blueTwenty.print();
- 
-	return 0;
+    Ball def;
+    std::cout << def;
+
+    Ball blue{"blue"};
+    blue.print();
+
+    Ball twenty{20.0};
+    twenty.print();
+
+    Ball blueTwenty{"blue", 20.0};
+    blueTwenty.print();
+
+    return 0;
 }
